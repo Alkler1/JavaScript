e8b45024e0e0675a34692994a4418b3d03e782fb
Mateus Machado <165442930+Alkler1@users.noreply.github.com>
2025-01-20 23:38:13 -0300

    Add files via upload

---
+++ ex001/app.js
@@ -0,0 +1,9 @@
+var torradas = 5 
+if(torradas > 0)
+    print("temos torradas :")
+else:
+print("não temos torradas :(")
+
+window.alert('Minha primeira mensagem!')
+window.confirm('está gostando de JS?') // Janela com botão ok e cance
+window.prompt('Qual é seu nome?') //vai perguntar o nome...
